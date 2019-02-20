afbbffed8ba66c5df99d4ce31afbf089c563b2c6
MaxHuw <m.huw.graham@gmail.com>
2019-02-20 01:45:22 +0000

    Exercise 5 done.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total revenue: #{Store.sum(:annual_revenue)}"
+puts "Average store revenue: #{Store.sum(:annual_revenue) / Store.count}"
+
+@stores_with_1m = Store.where("annual_revenue > 1000000")
+puts "Number of stores with revenue over 1 million: #{@stores_with_1m.size}"
+
+@stores_with_1m.each {|store| puts "Store name: #{store.name}, Revenue: #{store.annual_revenue}"}
